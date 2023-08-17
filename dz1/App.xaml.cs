3d40aeee5d67886d78297a32b464d0772ef8e4ac
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-08-17 22:13:55 +0200

    Add files via upload

---
+++ dz1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dz1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const String ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Startklar\\Desktop\\Новая папка\\dz1\\dz1\\SocialNetwork.mdf;Integrated Security=True";
+    }
+}
